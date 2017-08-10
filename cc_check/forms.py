61097a3d0f6509b3152047fd8ffa0e41303eb7a0
luheeslo <luishenriqueestrela@gmail.com>
2017-08-10 08:57:46 -0300

    accepting only .txt format

--- cc_check/forms.py
+++ cc_check/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 from wtforms import SubmitField
 
 
 class CCCForm(FlaskForm):
-    numbers = FileField("Upload your credit card numbers:", validators=[FileRequired()])
+    numbers = FileField("Upload your credit card numbers:", validators=[
+        FileRequired(),
+        FileAllowed(['txt'], '.txt format only!')])
     submit = SubmitField('Submit')
